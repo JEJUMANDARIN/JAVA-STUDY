fdda7b8f64598ca12b553376efd28ec8efa27cd4
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-18 23:19:28 +0900

    java study

--- practice_03.java
+++ practice_03.java
@@ -10,6 +10,7 @@ public class practice_03 {
         String s2 = "19920405";
         System.out.println("s2 = " + s2);
 
+
 //      1-1. equals
         String s3 = "HI";
         String s4 = "HI";
@@ -27,25 +28,55 @@ public class practice_03 {
         System.out.println(s6.indexOf("!", s6.indexOf("!") + 1));
 
 
-
 //      1-3. replace
         String s7 = s6.replace("GYURI", "SUBIN");
         System.out.println("s7 = " + s7);
 
+
 //      1-4. substring
+        System.out.println(s7.substring(0,5));
+        System.out.println(s7.substring(0, s7.indexOf("!")+1));
 
 
 //      1-5. toUpperCase
-
+        System.out.println(s7.toUpperCase());
 
 
 //      2. 자료형 - StringBuffer
         System.out.println("== StringBuffer ==");
+        StringBuffer sb1 = new StringBuffer();
+        sb1.append("01234");
+        System.out.println("sb1 = " + sb1);
+        sb1.append("56789");
+        System.out.println("sb1 = " + sb1);
 
+        String z = "01234";
+        String b = "56788";
+        String bak = z;
+        System.out.println(z == bak);
 
+        z += b;
+        System.out.println(z);
+        System.out.println(z == bak);
+        
 
 //      3. 자료형 - 배열
         System.out.println("== 배열 ==");
+        int[] myArray1 = {1,2,3,4,5};
+        System.out.println(myArray1[0]);
+        System.out.println(myArray1[1]);
+        System.out.println(myArray1[2]);
+        System.out.println(myArray1[3]);
+        System.out.println(myArray1[4]);
+
+        char[] myArray2 = {'a','b','c','d','e'};
+        System.out.println(myArray2[2]);
+
+        String[] myArray3 = new String[3];
+        myArray3[0] = "Hello";
+        myArray3[1] = " ";
+        myArray3[2] = "World";
+        System.out.println(myArray3[0] + myArray3[1] + myArray3[2]);
 
 
     }
