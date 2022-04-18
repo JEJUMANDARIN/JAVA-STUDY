2c853cf1b579879595bdaef61bc8ac720edf6971
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-18 20:20:32 +0900

    0418

--- practice_0418.java
+++ practice_0418.java
@@ -1,16 +1,15 @@
 // Java 프로그래밍 - 변수와 자료형_1
 
-public class Main0418 {
+public class practice_0418 {
     public static void main(String[] args) {
 
 //      1. 변수 사용하기
         System.out.println("== 변수 사용하기 ==");
         int age = 10;
+        System.out.println(age);
         String country = "Korea";
+        System.out.println(country);
 
-        System.out.println("age = " + age);
-        System.out.println("country = " + country);
-        System.out.println();
 
 //      2. 변수 이름 규칙
         System.out.println("== 변수 이름 규칙 ==");
@@ -19,58 +18,38 @@ public class Main0418 {
         int apple3 = 2000;
         int _apple = 2000;
         int $apple = 2000;
-
-        System.out.println("apple = " + apple);
-        System.out.println("apple3 = " + apple3);
-        System.out.println("_apple = " + _apple);
+        System.out.println($apple);
         System.out.println("$apple = " + $apple);
-        System.out.println();
-
 
 //      2-2. 숫자로 시작 X
 //        int 3apple = 2000;
 
-
 //      2-3. 대소문자 구분
-        int apple1 = 2000;
-        int Apple1 = 3000;
-
-        System.out.println("apple1 = " + apple1);
-        System.out.println("Apple1 = " + Apple1);
-        System.out.println();
+        int apple5 = 1000;
+        int Apple5 = 2000;
 
 //      2-4. 공백 사용 X
-//        int one apple = 2000;
-        int one_apple = 2000;
-        int oneApple = 2000;
-
+        int one_apple = 1000;
+        int oneApple = 1000;
 
 //      2-5. 예약어 사용 X
 //      예약어 예시: true, false, if, switch, for, continue, break, ...
-//        int true = 1;
-//        int false = 0;
-//        int if = 1;
-//        int continue = 10;
-
+//        int true =1;
 
 //      참고) 한글 사용 가능
-        int 나이 = 20;
-        System.out.println("나이 = " + 나이);
-
+        int 사과 = 1000;
+        System.out.println("사과 = " + 사과);
+//   한글은 가급적 사용하지 않는다
 
 //      3. 표기법
-        System.out.println("== 표기법 ==");
 //      3-1. 카멜 표기법 (camelCase)
 //      변수, 함수
-        int myAge = 10;
-        int oneApplePrice = 1000;
 
 
 //      3-2. 파스칼 표기법 (PascalCase)
 //      클래스
         int MyAge = 10;
         int OneApplePrice = 1000;
-
 
 //      참고) 스네이크 표기법 (snake_case)
 //      사용 X
