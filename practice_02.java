f2b00bcb3b4912550379aa6535e98ba386060ae5
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-18 20:57:14 +0900

    java study

---
+++ practice_02.java
@@ -0,0 +1,41 @@
+// Java 프로그래밍 - 변수와 자료형_2
+
+public class practice_02 {
+        public static void main(String[] args) {
+
+//      1. 자료형 - 숫자
+            System.out.println("== 숫자 ==");
+
+//      1-1. 정수
+            int intNum = 10;
+            System.out.println("intNum = " + intNum);
+
+            System.out.println(Integer.MAX_VALUE);
+            System.out.println(Integer.MIN_VALUE);
+
+            int intNum2 = Integer.MAX_VALUE;
+            System.out.println("intNum2 = " + intNum2);
+            int intNum3 = Integer.MAX_VALUE;
+            System.out.println("intNum3 = " + intNum3);
+            long longNum = (long)Integer.MAX_VALUE + 1;
+            System.out.println("longNum = " + longNum);
+
+//      1-2. 실수
+            float floatNum = 1.23f;
+            double doubleNum = 1.23;
+            System.out.println(Float.MAX_VALUE);
+            System.out.println(Double.MAX_VALUE);
+
+//      1-3. 2진수 / 8진수 / 16진수
+
+
+//      2. 자료형 - 부울
+            System.out.println("== 부울 ==");
+
+
+//      3. 자료형 - 문자
+            System.out.println("== 문자 ==");
+
+
+        }
+    }
