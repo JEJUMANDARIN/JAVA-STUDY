bca58c38806a9a2ca6548b192c72d6c3074d5302
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-25 20:37:06 +0900

    java study

---
+++ practice_10.java
@@ -0,0 +1,62 @@
+// Java 프로그래밍 - 클래스와 객체_1
+
+class Car {
+    String name;
+    String type;
+
+    public void printCarInfo() {
+        System.out.println("=== Car Info ===");
+        System.out.println("name: " + name);
+        System.out.println("type: " + type);
+    }
+
+    public void move() {
+        System.out.println("이동!");
+    }
+
+    public void brake() {
+        System.out.println("정지!");
+    }
+}
+
+class Car2 {
+    String name;
+    String type;
+
+    //    Car2() {}
+    Car2(String name, String type) {
+        this.name = name;
+        this.type = type;
+        System.out.println("생성자 출력!");
+    }
+
+    public void printCarInfo() {
+        System.out.println("=== Car Info ===");
+        System.out.println("name: " + name);
+        System.out.println("type: " + type);
+    }
+
+    public void load() {
+        System.out.println("짐을 주세요!");
+    }
+
+    public void horn() {
+        System.out.println("빵빵!");
+    }
+}
+
+public class practice_10 {
+    public static void main(String[] args) {
+        Car myCar1 = new Car();
+        myCar1.name = "a";
+        myCar1.type = "suv";
+        myCar1.printCarInfo();
+        myCar1.move();
+        myCar1.brake();
+
+        Car2 myCar2 = new Car2("b", "sedan");
+        myCar2.printCarInfo();
+        myCar2.load();
+        myCar2.horn();
+    }
+}
---
+++ practice_11.java
@@ -0,0 +1,51 @@
+//  Practice
+//  다음 클래스 직접 만든 후 객체 생성
+//  클래스명: Animal
+//  특성: 이름, 무게, 분류
+//  기능: 먹기, 잠자기, 걷기, 뛰기
+
+class Animal {
+    String name;
+    Double weight;
+    String classification;
+
+    Animal(String name, Double weight, String classification) {
+        this.name = name;
+        this.weight = weight;
+        this.classification = classification;
+    }
+
+    public void printInfo() {
+        System.out.println("name = " + name);
+        System.out.println("weight = " + weight);
+        System.out.println("classification = " + classification);
+    }
+
+    public void eat() {
+        System.out.println("냠냠!");
+    }
+
+    public void sleep() {
+        System.out.println("쿨쿨!");
+    }
+
+    public void walk() {
+        System.out.println("걷기!");
+    }
+
+    public void run() {
+        System.out.println("뛰기!");
+    }
+}
+
+public class practice_11 {
+    public static void main(String[] args) {
+        // Test code
+        Animal animal1 = new Animal("강아지", 5.0, "포유류");
+        Animal animal2 = new Animal("구피", 0.01, "어류");
+
+        animal1.printInfo();
+        animal2.printInfo();
+    }
+}
+
